2f33b7ef9e6cbdd26746a6cdeef673c8cf467697
Ulyana <ushinkevich@mail.ru>
2024-02-15 16:56:47 +0400

    setup routing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { AppGlobal } from './App.styles.js';
+import { AppRoutes } from './routes.jsx';
 
 function App() {
     return (
         <>
             <AppGlobal />
+            <AppRoutes />
+            <h1>app</h1>
         </>
     );
 }
