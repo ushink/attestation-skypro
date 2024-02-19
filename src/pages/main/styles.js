a0c9c6d5835a1e43c8cc1f6a1163dee11d195d84
Ulyana <ushinkevich@mail.ru>
2024-02-19 07:59:10 +0400

    ready page 404

--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -2,17 +2,6 @@ import styled from 'styled-components'
 
 export const wrapper = styled.div`
     height: 100vh;
-    background: linear-gradient(
-            rgba(135, 60, 255, 0.4),
-            rgba(135, 60, 255, 0) 80%
-        ),
-        linear-gradient(
-            -45deg,
-            rgba(120, 155, 255, 0.9) 25%,
-            rgba(255, 160, 65, 0.9) 75%
-        );
-    background-repeat: no-repeat;
-    background-size: cover;
     display: flex;
     flex-direction: column;
     align-items: center;
