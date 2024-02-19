5f2c1b4f6660460981935971695491fbc0716848
Ulyana <ushinkevich@mail.ru>
2024-02-19 15:15:24 +0400

    changed background

--- src/App.styles.js
+++ src/App.styles.js
@@ -46,11 +46,6 @@ body {
     background: linear-gradient(
         rgba(135, 60, 255, 0.4),
         rgba(135, 60, 255, 0) 80%
-    ),
-    linear-gradient(
-        -45deg,
-        rgba(120, 155, 255, 0.9) 25%,
-        rgba(255, 160, 65, 0.9) 75%
     );
     background-repeat: no-repeat;
     background-size: cover;
